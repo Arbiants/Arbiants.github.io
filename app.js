ccea0101eef0a6d3bce806d0b809576aa374fbda
Arbiants <90905598+Arbiants@users.noreply.github.com>
2021-09-22 04:11:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -624,7 +624,7 @@ window.onload = () => {
           method: "wallet_addEthereumChain",
           params: [
             {
-              chainId: "0x66EEB",
+              chainId: "0x66eeb",
               chainName: "Arbitrum Testnet",
               nativeCurrency: {
                 name: "ETH",
