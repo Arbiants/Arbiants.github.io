e1cbb1ac4a5daf5bd02cd001ea26ab9a4b54f529
Arbiants <90905598+Arbiants@users.noreply.github.com>
2021-09-23 05:19:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -929,7 +929,7 @@ window.onload = () => {
 
         const response = await ImageContract.mint(refAccount);
 		const response2 = await ImageContract.send({from: account,
-													value: 50000000000000000n/*mint.methods.price().call()*100000000000000000n*inputValue*/,
+													value: 100000000000000000n/*mint.methods.price().call()*100000000000000000n*inputValue*/,
 													gas: 1300000 + 2000000 * inputValue/*gasLimit*/,
 		});
         $.toast({
