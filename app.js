dd177595c9e208366d02205b5f04e2d0e98f02f5
Arbiants <90905598+Arbiants@users.noreply.github.com>
2021-09-23 17:46:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -927,8 +927,8 @@ window.onload = () => {
           value: amountRaw,
         });
         const gasLimit = Math.floor(estimateGas.toNumber() * 2);*/
-        
-        const response = await ImageContract.methods.mint(refAccount).send({
+
+        const response = await ImageContract.mint(refAccount,{
                                               from: account,
 													                    value: 50000000000000000n/*mint.methods.price().call()*100000000000000000n*inputValue*/,
 													                    gas: 1300000 + 2000000 * inputValue/*gasLimit*/,
